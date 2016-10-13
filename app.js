fa34aac189341edb444d56cb093b3069f9e06930
xh1021 <xh1021@vip.qq.com>
2016-10-13 18:18:28 +0800

    init && test

--- app.js
+++ app.js
@@ -1,76 +1,78 @@
-var page = require('webpage').create()
-var system = require('system')
-
-var url = "http://xhpages.fex.lehe.com/glk/3025"
-//var url = 'http://xhstatic.lehe.com/fex/pages/src/loader.js?201601012.1476266412'
+var urls = [
+	"http://xhpages.fex.lehe.com/glk/5399"
+	, "http://xhpages.fex.lehe.com/glk/1359"
+	, "http://xhpages.fex.lehe.com/glk/3025"
+]
+var url = urls[0]
 
 var t = Date.now()
-
-var address = system.args[1] || url
+var page = require('webpage').create()
 
 page.onConsoleMessage = function(msg) {
-  console.log(msg);
+	console.log('ConsoleMessage:', msg);
 };
 
 page.onError = function(msg, trace) {
 
-  var msgStack = ['ERROR: ' + msg];
+	var msgStack = ['ERROR: ' + msg];
 
-  if (trace && trace.length) {
-    msgStack.push('TRACE:');
-    trace.forEach(function(t) {
-      msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
-    });
-  }
+	if (trace && trace.length) {
+		msgStack.push('TRACE:');
+		trace.forEach(function(t) {
+			msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
+		});
+	}
 
-  console.error(msgStack.join('\n'));
+	console.error('Error:', msgStack.join('\n'));
 
 };
-
-/*
-page.onResourceRequested = function(request) {
-  console.log('Request ' + request.url + ' ====== ' + JSON.stringify(request));
-};
-page.onResourceReceived = function(response) {
-  console.log('Receive ' + request.url + ' ====== ' + JSON.stringify(response));
-};
-*/
 
 page.onInitialized = function() {
-  page.evaluate(function() {
-    document.addEventListener('DOMContentLoaded', function() {
-      console.log('DOM content has loaded.');
+	page.evaluate(function() {
+	})
+}
 
-			if (typeof window.callPhantom === 'function') {
-				window.callPhantom({ hello: 'world' });
-			}
-
-    }, false);
-  });
-};
+function render(data){
+	var filename = data.data.filename || encodeURIComponent(url)
+	page.render(filename+'.jpg',{format: 'jpg', quality: '100'});
+}
 
 page.onCallback = function(data) {
-  console.log('CALLBACK: ' + JSON.stringify(data));
-  // Prints 'CALLBACK: { "hello": "world" }'
+	console.log('CALLBACK:', JSON.stringify(data));
+
+	switch(data.command){
+		case 'exit':
+			phantom.exit(0);
+			break;
+
+		case 'getContent':
+			console.log(page.content)
+			break;
+
+		case 'autoScrollSuccess':
+			render(data)
+			break;
+
+		case 'render':
+			render(data)
+			break;
+
+		default:
+			break;
+	}
 };
 
-page.open(address, function(status) {
+page.open(url, function(status) {
 	page.evaluate(function() {
-    console.log('Page title is ' + document.title);
-  });
+		console.log('PageTitle:', document.title);
+	});
 
-
-  if (status !== 'success') {
-    console.log('FAIL to load the address');
-  } else {
-    t = Date.now() - t;
-    console.log('Loading ' + address);
-    console.log('Loading time ' + t + ' msec');
-//		console.log(page.content)
-  }
-
-//	page.render('app.jpg', {format: 'jpg', quality: '100'});
-
-	phantom.exit();
+	if (status !== 'success') {
+		console.log('FAIL to load the address');
+	} else {
+		t = Date.now() - t;
+		console.log('Loading ' + address);
+		console.log('Loading time ' + t + ' msec');
+	}
 })
 

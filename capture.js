f937fc97d8548fc198c96e1dbc5f7078e87fc4d9
许 慧 <xh1021@vip.qq.com>
2016-10-14 01:13:16 +0800

    split function

---
+++ capture.js
@@ -0,0 +1,57 @@
+var fs = require('fs');
+var system = require('system')
+var page = require('webpage').create()
+
+var url = system.args[1]
+
+var t = Date.now()
+
+page.onConsoleMessage = function(msg) {
+	console.log('ConsoleMessage:', msg);
+};
+
+page.onError = function(msg, trace) {
+
+	var msgStack = ['ERROR: ' + msg];
+
+	if (trace && trace.length) {
+		msgStack.push('TRACE:');
+		trace.forEach(function(t) {
+			msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
+		});
+	}
+
+	console.error('Error:', msgStack.join('\n'));
+
+};
+
+page.onCallback = function(data) {
+	console.log('CALLBACK:', JSON.stringify(data));
+
+	switch(data.command){
+		case 'exit':
+			phantom.exit(0);
+			break;
+
+		case 'autoScrollSuccess':
+			var filename = data.data.filename || encodeURIComponent(url)
+			page.render(filename+'.jpg',{format: 'jpg', quality: '100'});
+
+			t = Date.now() - t;
+			console.log('Loading time ' + t + ' msec');
+
+			phantom.exit(0);
+
+			break;
+
+		default:
+			break;
+	}
+};
+
+page.open(url, function(status) {
+	page.evaluate(function() {
+		console.log('PageTitle:', document.title);
+	});
+})
+

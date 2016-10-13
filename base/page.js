73cc0bbc433205575af19e9b8259703fb6465147
许 慧 <xh1021@vip.qq.com>
2016-10-14 02:14:49 +0800

    magnolia

---
+++ base/page.js
@@ -0,0 +1,30 @@
+var webpage = require('webpage')
+
+exports.create = function(){
+	var page = webpage.create()
+
+	page.onConsoleMessage = function(msg) {
+		console.log('ConsoleMessage:', msg);
+	};
+
+	page.onError = function(msg, trace) {
+
+		var msgStack = ['ERROR: ' + msg];
+
+		if (trace && trace.length) {
+			msgStack.push('TRACE:');
+			trace.forEach(function(t) {
+				msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
+			});
+		}
+
+		console.error('Error:', msgStack.join('\n'));
+
+	};
+
+	page.onInitialized = function() {
+		page.evaluateJavaScript('function(){'+clientJS+'}')
+	}
+
+	return page
+}
--- dom.js
+++ dom.js
@@ -1,34 +1,11 @@
 var fs = require('fs');
 var system = require('system')
-var page = require('webpage').create()
+var page = require('./base/page.js').create()
 var clientJS = fs.read('./client/common.js')
 
 var url = system.args[1]
 
 var t = Date.now()
-
-page.onConsoleMessage = function(msg) {
-	console.log('ConsoleMessage:', msg);
-};
-
-page.onError = function(msg, trace) {
-
-	var msgStack = ['ERROR: ' + msg];
-
-	if (trace && trace.length) {
-		msgStack.push('TRACE:');
-		trace.forEach(function(t) {
-			msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
-		});
-	}
-
-	console.error('Error:', msgStack.join('\n'));
-
-};
-
-page.onInitialized = function() {
-	page.evaluateJavaScript('function(){'+clientJS+'}')
-}
 
 page.open(url, function(status) {
 	page.evaluate(function() {

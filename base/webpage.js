3199c2f01ca1abe63ab666ecf911996510a981a8
xh1021 <xh1021@vip.qq.com>
2016-10-14 12:54:03 +0800

    clientjs slite

--- base/webpage.js
+++ base/webpage.js
@@ -1,5 +1,16 @@
 var webpage = require('webpage')
-var clientJS = fs.read('./client/common.js')
+var fs = require('fs')
+
+var clientJS = ''
+var clientJSBase = './client'
+var clientJSList = fs.list(clientJSBase)
+
+for(var i in clientJSList){
+	var filePath = clientJSBase + '/' + clientJSList[i]
+	if(fs.isFile(filePath)){
+		clientJS += fs.read(filePath)
+	}
+}
 
 exports.create = function(){
 	var page = webpage.create()
